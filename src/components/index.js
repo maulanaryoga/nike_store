d012ceb9c30b76791fa667a2e3d5144521b89da4
Maulana Arya Yoga J <maulanayoga590@gmail.com>
2023-09-07 12:51:06 +0700

    first-commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import Nav from './Nav';
+import PopularProductCard from './PopularProductCard';
+import ShoeCard from './ShoeCard';
+
+export {
+    Nav,
+    Button,
+    PopularProductCard,
+    ShoeCard,
+};
